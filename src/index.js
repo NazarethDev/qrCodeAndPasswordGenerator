6e8433578fba96d317d0ac5c8b2aab33b7dcd4fb
NazarethDev <lorran.nazareth@gmail.com>
2025-07-30 21:28:07 -0300

    Funcionalidade para geração de senhas implementada com sucesso

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import prompt from "prompt"
 import mainPrompt from "./prompts/prompt-main.js";
 import chalk from "chalk";
 import createQRCode from "./service/qr-code/create.js";
+import createPassword from "./service/password/create.js";
 
 async function main() {
     prompt.get(mainPrompt, async (err, choose) => {
         if (choose.select == 1 ) await createQRCode();
-        if(choose.select == 2) console.log(chalk.blueBright.bold("escolheu password"));
+        if(choose.select == 2) await createPassword();
     });
     prompt.start();
 };
